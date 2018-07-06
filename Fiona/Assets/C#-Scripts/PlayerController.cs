5cec1b00037a4a9bcf3404e1568c331349180d6a
Dai Pham <phamxuandai94@googlemail.com>
2018-07-07 00:39:20 +0200

    PlayerController

---
+++ Fiona/Assets/C#-Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] private float max_speed = 3f ;
+    [SerializeField] private float max_jumpPower = 3f;
+    GameObject Animation;
+    Animator tmp;
+    // Use this for initialization
+    void Start () {
+        Animation = transform.GetChild(0).gameObject;
+        tmp = Animation.GetComponent<Animator>();
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * max_speed;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * max_jumpPower;
+
+        transform.Translate(x, y, 0);
+        Move(); 
+    }
+
+    void Move ()
+    { // 
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKeyDown(KeyCode.D) && Animation.transform.rotation.y != 0) {
+                transform.Rotate(0, 0, 0);
+                
+            }
+            tmp.SetInteger("State", 1);
+
+        }
+       else 
+       if (Input.GetKey(KeyCode.A))
+        {
+            if (Input.GetKeyDown(KeyCode.A) && Animation.transform.rotation.y != -1) {
+                transform.Rotate(0, 180, 0); }
+               
+            tmp.SetInteger("State", 1);
+            
+            
+        }   
+        else
+        {
+            tmp.SetInteger("State", 0);
+        }
+
+        Debug.Log(transform.rotation.y);
+    }
+}
