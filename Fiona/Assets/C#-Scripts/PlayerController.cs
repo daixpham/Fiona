2a638348dd561c7a1a5630499909aa2180e421d7
Dai Pham <phamxuandai94@googlemail.com>
2018-07-07 11:19:03 +0200

    PlayerController

--- Fiona/Assets/C#-Scripts/PlayerController.cs
+++ Fiona/Assets/C#-Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     [SerializeField] private float max_speed = 3f ;
     [SerializeField] private float max_jumpPower = 3f;
+    [SerializeField] private GameObject player;
     GameObject Animation;
     Animator tmp;
+    private float x;
+    private float y; 
     // Use this for initialization
     void Start () {
         Animation = transform.GetChild(0).gameObject;
@@ -18,39 +21,41 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * max_speed;
-        float y = Input.GetAxis("Vertical") * Time.deltaTime * max_jumpPower;
-
-        transform.Translate(x, y, 0);
-        Move(); 
-    }
-
-    void Move ()
-    { // 
+        float y_ = player.transform.rotation.y;
         if (Input.GetKey(KeyCode.D))
         {
-            if (Input.GetKeyDown(KeyCode.D) && Animation.transform.rotation.y != 0) {
-                transform.Rotate(0, 0, 0);
+            
+           
+            if (player.transform.rotation.y ==1)
+            {
+                transform.Rotate(0, 180, 0);
                 
             }
+
             tmp.SetInteger("State", 1);
 
         }
-       else 
-       if (Input.GetKey(KeyCode.A))
+        else
+      if (Input.GetKey(KeyCode.A))
         {
-            if (Input.GetKeyDown(KeyCode.A) && Animation.transform.rotation.y != -1) {
-                transform.Rotate(0, 180, 0); }
-               
+           
+            if (player.transform.rotation.y != 1)
+            {
+              
+                
+                transform.Rotate(0, 180, 0);
+                
+            }
+
             tmp.SetInteger("State", 1);
-            
-            
-        }   
+
+
+        }
         else
         {
             tmp.SetInteger("State", 0);
         }
-
-        Debug.Log(transform.rotation.y);
     }
+
+   
 }
