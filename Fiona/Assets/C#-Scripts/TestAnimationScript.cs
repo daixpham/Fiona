b47aaf5620315e57895be2a3a8d75e8127334211
Dai Pham <phamxuandai94@googlemail.com>
2018-07-07 21:21:29 +0200

    Player_Jump an Run

--- Fiona/Assets/C#-Scripts/TestAnimationScript.cs
+++ Fiona/Assets/C#-Scripts/TestAnimationScript.cs
@@ -9,18 +9,63 @@ public class TestAnimationScript : MonoBehaviour {
     private bool _jumping;
     private bool _landing;
     private bool _dead;
-
+    private bool _crouch;
+    private bool grounded;
     public Animator controller;
-	// Use this for initialization
-	void Start () {
-	}
+    public Rigidbody rigidBody;
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyUp(KeyCode.D))
         {
             _running = !_running;
             controller.SetBool("IsRunning", _running);
         }
-	}
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            
+            controller.SetBool("IsJumping", true);
+            
+            rigidBody.AddForce(new Vector3(0, 10f,0), ForceMode.Impulse);
+         
+            if (grounded == true )
+            {
+                controller.SetBool("IsJumping", false);
+                controller.SetBool("InLanding", false);
+
+            }
+        }
+
+      
+
+    }
+
+  
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+            
+            controller.SetBool("InLanding", true);
+
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = false;
+            _jumping = false;
+
+        }
+    }
 }
