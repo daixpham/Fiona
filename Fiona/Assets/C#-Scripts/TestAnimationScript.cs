e34027ef4acad88746a5c59845fcf90b3cf94aca
Lisa <lisa@acagamics.de>
2018-07-07 15:48:05 +0200

    Basic Animation from fiona

---
+++ Fiona/Assets/C#-Scripts/TestAnimationScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class TestAnimationScript : MonoBehaviour {
+
+    private bool _running;
+    public Animator controller;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            _running = !_running;
+            controller.SetBool("IsRunning", _running);
+        }
+	}
+}
