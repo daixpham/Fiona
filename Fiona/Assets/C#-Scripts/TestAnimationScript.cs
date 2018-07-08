04e4532cef5b527684cd1cd601d6d10ecce00ff4
Dai Pham <phamxuandai94@googlemail.com>
2018-07-08 12:48:33 +0200

    MergeConflict_testAnimationScript

--- Fiona/Assets/C#-Scripts/TestAnimationScript.cs
+++ Fiona/Assets/C#-Scripts/TestAnimationScript.cs
@@ -12,44 +12,61 @@ public class TestAnimationScript : MonoBehaviour {
 	private bool _crouch;
 	private bool grounded;
 	private bool w_pressed;
+    private int jump_count = 0;
 	public Animator controller;
 	public Rigidbody rigidBody;
-
-	// Use this for initialization
-	void Start () {
+    GameObject cameraObj;
+    // Use this for initialization
+    void Start () {
 		rigidBody = GetComponent<Rigidbody>();
-	}
+        cameraObj = transform.GetChild(11).gameObject;
+    }
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyUp(KeyCode.D))
-		{
-			_running = !_running;
-			controller.SetBool("IsRunning", _running);
-		}
+
+        if (GameSingelton.PlayerHealth > 0)
+        {
+            controller.SetBool("IsRunning", true);
 
 
-		if (Input.GetKeyDown(KeyCode.W) && !w_pressed)
-		{
-
-			controller.SetBool("IsJumping", true);
-
-			rigidBody.AddForce(new Vector3(0, 20f,0), ForceMode.Impulse);
 
 
-		}
+            if (Input.GetKeyDown(KeyCode.W) && !w_pressed && grounded)
+            {
 
-		if (grounded == true)
-		{
-			controller.SetBool("IsJumping", false);
-			controller.SetBool("InLanding", false);
+                controller.SetBool("IsJumping", true);
+                controller.SetBool("IsRunning", false);
+                rigidBody.AddForce(new Vector3(0, 25f, 0), ForceMode.Impulse);
+               
+            }
+            else
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                rigidBody.AddForce(new Vector3(0, 0f, 0), ForceMode.Impulse);
+                controller.SetBool("IsJumping", false);
+            }
 
-		}
+            if (grounded == true)
+            {
+                controller.SetBool("IsJumping", false);
+                controller.SetBool("InLanding", false);
+                
 
-		if (_jumping == true)
-		{
-			w_pressed = true;
-		}
+            }
+
+            if (_jumping == true)
+            {
+                w_pressed = true;
+                jump_count = jump_count - 1;
+            }
+        }else
+        {
+            controller.SetBool("IsRunning", false);
+            cameraObj.transform.parent = transform.parent;
+            if (transform.localScale.x>0)
+            transform.localScale -= new Vector3(Time.deltaTime, Time.deltaTime, Time.deltaTime);
+        }
 
 
 	}
